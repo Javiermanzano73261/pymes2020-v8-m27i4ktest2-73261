94575e29fb535d1095c5c6d53ff00737b9b78a9f
Javiermanzano73261 <manzanojavier@hotmail.com>
2020-11-08 08:52:23 -0300

    2 files modified, 4 added

---
+++ src/app/models/servicio.ts
@@ -0,0 +1,14 @@
+export class Servicio {
+  IdServicio: number;
+  Descripcion: string;
+  Importe: number;
+  CantidadHoras: number;
+  
+
+  /*Cada alumno va a desarrollar un proyecto con la Tabla Servicios
+idservicio (autoincremental)
+descripcion varchar(50)
+importe numeric(12,2)
+cantidadhoras int
+ */
+};
